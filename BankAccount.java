46154b8888b7439fc178116f078bcb3c123e4c4b
jedkutai <jedkutai@gmail.com>
2026-09-04 12:44:18 -0400

    withdraw safeguard a

--- BankAccount.java
+++ BankAccount.java
@@ -16,6 +16,10 @@ public class BankAccount {
         // Withdraw the requested amount if sufficient funds are available.
         // Return true if the withdrawal succeeds.
         // Return false otherwise. return false;
+        if (this.balance - amount >= 50) {
+            this.balance -= amount;
+            return true;
+        }
         return false;
     }
 
